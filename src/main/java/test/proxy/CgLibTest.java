6b13db1fa59e0db6426e59a74b1d7fdc5cbfe787
w-cross <wzw2984521645@163.com>
2018-04-07 15:40:57 +0800

    动态代理添加

---
+++ src/main/java/test/proxy/CgLibTest.java
@@ -0,0 +1,35 @@
+package test.proxy;
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class CgLibTest {
+    int getNumber(){
+        return 4;
+    }
+}
+
+class MyCglibTest implements MethodInterceptor{
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("代理类先执行");
+        Object object = methodProxy.invokeSuper(o,objects);
+        System.out.println(object);
+        return object;
+    }
+}
+
+class initCglibTes{
+    public static void main(String[] args) {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(CgLibTest.class);
+        enhancer.setCallback(new MyCglibTest());
+        CgLibTest cgLibTest = (CgLibTest)enhancer.create();
+        cgLibTest.getNumber();
+    }
+
+}
